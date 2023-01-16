fa8f6f8f3e5a98eba5538e0d67732b165604af84
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-16 17:09:34 -0300

    1 file modified

--- script.js
+++ script.js
@@ -144,6 +144,15 @@ if((nombreIngresado == "ANA") || (nombreIngresado =="ana")){
 }
 */
 
+
+
+
+
+
+
+
+
+
 // Clase 3
 /*
 for (let i = 0; i < 10; i++) {
@@ -222,6 +231,15 @@ while(entrada != "ESC" ){
    entrada = prompt("Ingresar un nombre");
 }
 */
+
+
+
+
+
+
+
+
+//Clase 4
 /*
 function saludar() {
   console.log("¡Hola estudiantes!");
@@ -314,6 +332,7 @@ const resta = (a, b) =>  a - b ;
 console.log( suma(15,20) )
 console.log( resta(20,5) )
 */
+/*
 const suma  = (a,b) => a + b
 const resta = (a,b) => a - b
 //Si una función es una sola línea con retorno y un parámetro puede evitar escribir los ()
@@ -323,3 +342,112 @@ let descuento = 50
 //Calculo el precioProducto + IVA - descuento
 let nuevoPrecio = resta(suma(precioProducto, iva(precioProducto)), descuento) 
 console.log(nuevoPrecio)
+*/
+
+
+
+
+
+
+// Clase 5
+/*
+let nombre = "Homero";
+let edad   = 39;
+let calle  = "Av. Siempreviva 742";
+// Los variables anteriores entran relacionados entre sí, entonces mejor usamos un objeto literal
+const persona1 = { nombre: "Homero", edad: 39, calle: "Av. Siempreviva 742" }
+console.log(persona1.nombre)
+console.log(persona1.edad)
+console.log(persona1.calle)
+console.log(persona1["nombre"])
+console.log(persona1["edad"])
+console.log(persona1["calle"])
+persona1["nombre"] = "Marge"
+persona1.edad = 36
+console.log(persona1.nombre)
+console.log(persona1.edad)
+console.log(persona1.calle)
+*/
+/*
+function Persona(nombre, edad, calle) {
+  this.nombre = nombre;
+  this.edad 	 = edad;
+  this.calle  = calle;
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+const persona2 = new Persona("Marge", 36, "Av. Siempreviva 742");
+*/
+/*
+function Persona(literal) {
+  this.nombre = literal.nombre;
+  this.edad   = literal.edad;
+  this.calle  = literal.calle;
+}
+const persona1 = new Persona({ nombre: "Homero", edad: 39, calle: "Av.Siempreviva 742" });
+*/
+/*
+// Métodos en objetos JS
+let cadena = "HOLA CODER";
+//Propiedad de objeto String: Largo de la cadena.
+console.log(cadena.length);
+//Método de objeto String: Pasar a minúscula.
+console.log(cadena.toLowerCase());
+//Método de objeto String: Pasar a mayúscula.
+console.log(cadena.toUpperCase());
+// Metodos
+function Persona(nombre, edad, calle) {
+  this.nombre = nombre;
+  this.edad   = edad;
+  this.calle  = calle;
+  this.hablar = function(){ console.log("HOLA SOY "+ this.nombre)}
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+const persona2 = new Persona("Marge", 36, "Av. Siempreviva 742");
+persona1.hablar();
+persona2.hablar();
+
+//devuelve true porque la clave "nombre" existe en el objeto persona1
+console.log( "nombre" in persona1);
+//devuelve false porque la clave "origen" no existe en el objeto persona1
+console.log( "origen" in persona1);
+//recorremos todas las propiedades del objeto con el ciclo for...in
+for (const propiedad in persona1) {
+    console.log(persona1[propiedad]);
+}
+*/
+//clases en js
+
+class Persona{
+  constructor(nombre, edad, calle) {
+      this.nombre = nombre;
+      this.edad   = edad;
+      this.calle  = calle;
+  }
+  hablar(){
+      console.log("HOLA SOY "+ this.nombre);
+  }
+}
+const persona1 = new Persona("Homero", 39, "Av. Siempreviva 742");
+persona1.hablar();
+
+class Producto {
+  constructor(nombre, precio) {
+      this.nombre  = nombre.toUpperCase();
+      this.precio  = parseFloat(precio);
+      this.vendido = false;
+  }
+  sumaIva() {
+      this.precio = this.precio * 1.21;
+  }
+  vender() {
+      this.vendido = true;
+  }
+}
+const producto1 = new Producto("arroz", "125");
+const producto2 = new Producto("fideo", "50");
+producto1.sumaIva();
+producto2.sumaIva();
+producto1.vender();
+console.log(producto1.vendido)
+console.log(producto1.precio)
+console.log(producto2.precio)
