a3025df1b7f41d83ee65cba470a17b0aae4828c4
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-17 16:11:29 -0300

    1 file modified

--- clase7FuncionesOrdenSuperior.js
+++ clase7FuncionesOrdenSuperior.js
@@ -24,7 +24,7 @@ let resta = asignarOperacion("restar")
 console.log( suma(4, 6) )  //  10
 console.log( resta(5, 3) )  //  2
 */
-
+/*
 //Recibir funciones por parámetro
 function porCadaUno(arr, fn) {
   for (const el of arr) {
@@ -53,5 +53,51 @@ porCadaUno(numeros, (el)=> {
 })
 
 console.log(duplicado) // [2, 4, 6, 8]
-
+*/
 //Métodos de búsqueda y transformación
+
+
+// 1) El método forEach() es similar al ejemplo porCadaUno anterior. Itera sobre el array y por cada elemento ejecuta la función que enviemos por parámetro, la cual recibe a su vez el elemento del array que se está recorriendo:
+const numeros = [1, 2, 3, 4, 5, 6]
+numeros.forEach( (num)=> {
+    console.log(num)
+} )
+
+// 2) El método find() recibe una función de comparación por parámetro. Captura el elemento que se está recorriendo y retorna true o false según la comparación ejecutada. El método retorna el primer elemento que cumpla con esa condición:
+
+const cursos = [
+  {nombre: 'Javascript', precio: 15000},
+  {nombre: 'ReactJS', precio: 22000},
+  {nombre: 'AngularJS', precio: 22000},
+  {nombre: 'Desarrollo Web', precio: 16000},
+]
+
+
+const resultado = cursos.find((el) => el.nombre === "ReactJS")
+const resultado2 = cursos.find((el) => el.nombre === "DW")
+
+console.log(resultado) // {nombre: 'ReactJS', precio: 22000}
+console.log(resultado2) // undefined
+
+//si modifico la variable creada a partir del find tambien modifico el array original (quedan atados por que las dos variables apuntan a la misma posicion de memoria)
+resultado.nombre = "maxiJS"
+console.log(cursos)
+
+// 3) El método filter() recibe, al igual que find(), una función comparadora por parámetro, y retorna un nuevo array con todos los elementos que cumplan esa condición. Si no hay coincidencias, retornará un array vacío.
+const resultado3 = cursos.filter((el) => el.nombre.includes('JS'))
+const resultado4 = cursos.filter((el) => el.precio < 14000)
+
+console.log(resultado3)
+console.log(resultado4) 
+
+//si modifico la variable creada a partir del filter tambien modifico el array original (quedan atados por que las dos variables apuntan a la misma posicion de memoria)
+resultado3[0].precio = 0
+console.log(cursos)
+
+//4 El método some() funciona igual que el find() recibiendo una función de búsqueda. En vez de retornar el elemento encontrado, some() retorna true o false según el resultado de la iteración de búsqueda.
+console.log( cursos.some((el) => el.nombre == "Desarrollo Web")) 
+// true
+console.log( cursos.some((el) => el.nombre == "VueJS")) 
+// false
+
+//5) El método map() crea un nuevo array con todos los elementos del original transformados según las operaciones de la función enviada por parámetro. Tiene la misma cantidad de elementos pero los almacenados son el return de la función:
