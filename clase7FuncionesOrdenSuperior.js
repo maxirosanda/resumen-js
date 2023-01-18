93221e8a5c766cc53f19d4efd54c074995a8a81b
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-18 11:43:33 -0300

    1 file modified

--- clase7FuncionesOrdenSuperior.js
+++ clase7FuncionesOrdenSuperior.js
@@ -165,8 +165,9 @@ numeros3.sort((a,b) => b - a)
 
 const sort = (arr,fs) =>{
   for(let i = 0 ; i < arr.length ; i++){
-    for(let j = arr.length ; j >= 0 ; i--){
+    for(let j = arr.length ; j >= 0 ; j--){
       console.log(i ,j)
     }
   }
 }
+sort([1,233,45],32)
