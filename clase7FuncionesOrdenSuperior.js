15c5944c70fb39dc201c2a018219c34ca42ae958
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-18 21:45:32 -0300

    1 file modified

--- clase7FuncionesOrdenSuperior.js
+++ clase7FuncionesOrdenSuperior.js
@@ -189,15 +189,15 @@ console.log(numeros3)
 */
 
 
-const productos = [{ id: 1,  producto: "Arroz", precio: 125 },
-                  {  id: 2,  producto: "Fideo", precio: 70 },
-                  {  id: 3,  producto: "Pan"  , precio: 50},
-                  {  id: 4,  producto: "Flan" , precio: 100}]
+const productos = [{ id: 1,  nombre: "Arroz", precio: 125 },
+                  {  id: 2,  nombre: "Fideo", precio: 70 },
+                  {  id: 3,  nombre: "Pan"  , precio: 50},
+                  {  id: 4,  nombre: "Flan" , precio: 100}]
 
 const buscado = productos.find(producto => producto.id === 3)
 console.log(buscado) //{id: 3, producto: "Pan", precio: 50}
 
-const existe = productos.some(producto => producto.nombre === “Harina”)
+const existe = productos.some(producto => producto.nombre === "Harina")
 console.log(existe ) // false
 
 const baratos = productos.filter(producto => producto.precio < 100)
